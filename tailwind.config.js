596cb23aa4ea341e428472fd63309d7c31e7916f
zizozizo123km <nnacar12@gmail.com>
2026-01-07 19:45:15 +0100

    AI Deploy: tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class', // Enable class-based dark mode
+  darkMode: 'class', // Enable dark mode based on parent class
   theme: {
     extend: {},
   },
